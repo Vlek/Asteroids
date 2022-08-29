53e098ee5cdbeea7c63f8e86c2d83b7b68649941
vlek <dmccammond25@gmail.com>
2022-08-28 22:34:54 -0700

    Dodge the creeps tutorial done!

--- src/Asteroids/Main.cs
+++ src/Asteroids/Main.cs
@@ -16,6 +16,7 @@ public class Main : Node
 	public override void _Ready()
 	{
 		GD.Randomize();
+		NewGame();
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,4 +24,60 @@ public class Main : Node
 //  {
 //      
 //  }
+
+	public void GameOver()
+	{
+		GetNode<Timer>("MobTimer").Stop();
+		GetNode<Timer>("ScoreTimer").Stop();
+		
+		GetNode<HUD>("HUD").ShowGameOver();
+	}
+
+	public void NewGame()
+	{
+		Score = 0;
+		
+		var player = GetNode<Player>("Player");
+		var startPosition = GetNode<Position2D>("StartPosition");
+		player.Start(startPosition.Position);
+		
+		GetNode<Timer>("StartTimer").Start();
+		
+		var hud = GetNode<HUD>("HUD");
+		hud.UpdateScore(Score);
+		hud.ShowMessage("Get Ready!");
+	}
+	
+	public void OnScoreTimerTimeout()
+	{
+		Score++;
+		
+		GetNode<HUD>("HUD").UpdateScore(Score);
+	}
+	
+	public void OnStartTimerTimeout()
+	{
+		GetNode<Timer>("MobTimer").Start();
+		GetNode<Timer>("ScoreTimer").Start();
+	}
+	
+	public void OnMobTimerTimeout()
+	{
+		var mob = (Mob)MobScene.Instance();
+		
+		var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+		mobSpawnLocation.Offset = GD.Randi();
+		
+		float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+		
+		mob.Position = mobSpawnLocation.Position;
+		
+		direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+		mob.Rotation = direction;
+		
+		var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+		mob.LinearVelocity = velocity.Rotated(direction);
+		
+		AddChild(mob);
+	}
 }
